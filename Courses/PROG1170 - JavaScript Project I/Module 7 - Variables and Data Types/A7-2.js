8156c23ed10194bfccae627179562de5b03d2ca7
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-01-14 17:58:30 -0700

    feat: Completed PROG1170 Module 7 A7-2

---
+++ Courses/PROG1170 - JavaScript Project I/Module 7 - Variables and Data Types/A7-2.js
@@ -0,0 +1,3 @@
+document.write("Lois said, \"I would love to have a juicy cheeseburger!\"");
+document.write("<br><br>");
+document.write("Superman replied, \"Not now, Later! I am busy saving the world!\"");
