50cf5149331819a24619b3c622d9bbbb60679259
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-01-15 09:32:01 -0700

    feat: Completed PROG1170 Module 7 A7-2, again

--- Courses/PROG1170 - JavaScript Project I/Module 7 - Variables and Data Types/A7-2.js
+++ Courses/PROG1170 - JavaScript Project I/Module 7 - Variables and Data Types/A7-2.js
@@ -1,3 +1,4 @@
-document.write("Lois said, \"I would love to have a juicy cheeseburger!\"");
-document.write("<br><br>");
-document.write("Superman replied, \"Not now, Later! I am busy saving the world!\"");
+let loisDialogue = '<p>Lois said, "I would love to have a juicy cheeseburger!"</p>',
+supermanDialogue = '<p>Superman replied, "Not now, Later! I am busy saving the world!"</p>';
+document.write(loisDialogue);
+document.write(supermanDialogue);
--- Courses/PROG1170 - JavaScript Project I/Module 7 - Variables and Data Types/A7-2_tl.js
+++ Courses/PROG1170 - JavaScript Project I/Module 7 - Variables and Data Types/A7-2_tl.js
@@ -1,3 +1,5 @@
-document.write(`Lois said, "I would love to have a juicy cheeseburger!"`);
-document.write("<br><br>");
-document.write(`Superman replied, "Not now, Later! I am busy saving the world!"`);
+let loisDialogue = `Lois said, "I would love to have a juicy cheeseburger!"`,
+supermanDialogue = `Superman replied, "Not now, Later! I am busy saving the world!"`;
+document.write(loisDialogue);
+document.write("<br><br>")
+document.write(supermanDialogue);
