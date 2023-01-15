439e25cafb17ed9b707c4da12f177b0e0712f3d3
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-01-15 09:09:14 -0700

    feat: Completed PROG1170 Module 7 A7-1 Book Exercises

--- Courses/PROG1170 - JavaScript Project I/Module 7 - Variables and Data Types/prjs3_2.js
+++ Courses/PROG1170 - JavaScript Project I/Module 7 - Variables and Data Types/prjs3_2.js
@@ -1,8 +1,8 @@
 var myheading = "<h1>This is My Web Page!</h1>",
 linktag = "<a href=\"http://www.webxpertz.net\">Web Site Link!</a>",
 sometext = "This text can be affected by other statements.",
-begineffect = "<em>",
-endeffect = "</em>",
+begineffect = "<strong>",
+endeffect = "</strong>",
 beginpara = "<p>",
 endpara = "</p>";
 document.write(myheading);
--- Courses/PROG1170 - JavaScript Project I/Module 7 - Variables and Data Types/prjs3_2_tl.js
+++ Courses/PROG1170 - JavaScript Project I/Module 7 - Variables and Data Types/prjs3_2_tl.js
@@ -1,11 +1,2 @@
-var myheading = "<h1>This is My Web Page!</h1>",
-linktag = "<a href=\"http://www.webxpertz.net\">Web Site Link!</a>",
-sometext = "This text can be affected by other statements.",
-begineffect = "<em>",
-endeffect = "</em>",
-beginpara = "<p>",
-endpara = "</p>";
-document.write(myheading);
-document.write(begineffect + sometext + endeffect);
-document.write(beginpara + linktag + endpara);
-document.write(beginpara + sometext + endpara);
+var sometext = "<h1>This is My Web Page!</h1><em>This text can be affected by other statements.</em><br><br><a href=\"http://www.webxpertz.net\">Web Site Link!</a><br><p>This text can be affected by other statements.</p>";
+document.write(sometext);
