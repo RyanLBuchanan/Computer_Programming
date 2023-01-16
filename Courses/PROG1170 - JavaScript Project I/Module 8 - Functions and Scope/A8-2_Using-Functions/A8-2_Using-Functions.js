e8d8cb807d7604134fe9c4cf033566a3bd0584f2
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-01-16 13:34:06 -0700

    feat: Completed PROG1170 Module 8 A8-2

---
+++ Courses/PROG1170 - JavaScript Project I/Module 8 - Functions and Scope/A8-2_Using-Functions/A8-2_Using-Functions.js
@@ -0,0 +1,12 @@
+// Step One
+let addNumbers = function(number1, number2) {
+  return number1 + number2
+  };
+let result1 = addNumbers(34, 8);
+document.write(result1);
+document.write("<br><br>")
+
+// Step Two
+let addNumbers2 = (number1, number2) => number1 + number2;
+let result2 = addNumbers2(76, 12);
+document.write(result2);
