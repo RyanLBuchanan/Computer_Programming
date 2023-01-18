6a4c38cfb6f5eeb41489bcd29600af693dbe23a0
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-01-18 10:27:12 -0700

    feat: Completed PROG1170 Module 10 quiz

---
+++ Courses/PROG1170 - JavaScript Project I/Module 10 - Conditionals and Loops/index.js
@@ -0,0 +1,85 @@
+// for (var count = 1, count < 11, count++)
+// {
+//   document.write( count + <br>)
+// }
+
+// var counter = 3;
+// do 
+// {
+//   document.write(counter + <br>);
+// }
+// while (counter > 11);
+
+// for (var i = 3; i < 11; i++) {
+//   document.write(i + "<br>");
+// }
+
+// var i = 3;
+// while ( i > 10){
+//   document.write(i + "<br>")
+// }
+
+// let myNum = "3";
+// if (myNum == 3)
+//     {  document.write( "The numbers are equal. " );  }
+// document.write( "The numbers are not equal. " ); 
+
+// for (let i = 100; i >= 0; i++)
+// {
+// document.write("This is line " + i + "<br>");
+// }
+
+// for (let i = 1; i >= 5; i--){
+//   document.write("Cats are cool!<br>");
+//   for (var x = 1; x > 0; x++){
+//     document.write("Dogs are loyal!");
+//   }
+//   document.write("<br>");
+// }
+
+// for (let i = 0; i <= 5; i++){
+//   document.write("Dogs are loyal!<br>");
+//   for (var x = 5; x > 0; x--){
+//     document.write("Cats are cool!");
+//   }
+//   document.write("<br>");
+// }
+
+// for (let i = 0; i <= 5; i++){
+//   document.write("Dogs are loyal!<br>");
+//   for (var x = 1; x > 0; x++){
+//     document.write("Cats are cool!");
+//   }
+//   document.write("<br>");
+// }
+
+// for (let i = 1; i <= 5; i++){
+//   document.write("Cats are cool!<br>");
+//   for (var x = 5; x > 0; x--){
+//     document.write("Dogs are loyal!");
+//   }
+//   document.write("<br>");
+// }
+
+// let myVariable = 1;
+
+// switch (myVariable) {
+//   case 1: 
+//       document.write("You chose case 1");
+//   case 2:
+//      document.write("You chose case 2");
+//   case 3:
+//      document.write("You chose case 3");
+//   default:
+//      document.write("This has fallen through to the default");
+//   } 
+
+let myLetter = "M";
+if (myLetter = "F")
+ {
+ document.write( "myLetter is F! " );
+ }
+ else
+ {
+ document.write( "myLetter is M! "  ); 
+ }
