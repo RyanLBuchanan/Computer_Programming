3771fa1f22c410b6b114fda9f85b6c8e94863772
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-01-18 17:57:23 -0700

    feat: Added PROG1170 Module 10 A10-1 Book Exercises templates

---
+++ Courses/PROG1170 - JavaScript Project I/Module 10 - Conditionals and Loops/A1-1_Book-Exercises/prjs6_1.js
@@ -0,0 +1,25 @@
+let msg = "";
+let city = window.prompt("What is the name of your city?", "");
+
+if ((city === null) || (city === "")) {
+  msg = "No city entered. Cannot determine delivery availability.";
+} else {
+  switch (city) {
+    case "Johnstown":
+      msg = "We can have items delivered to you in 3 days.";
+      break;
+    case "Donville":
+      msg = "We can have items delivered to you in 1 week.";
+      break;
+    case "Danieltown": // fall through
+    case "Martyville":
+      msg = "We can have items delivered to you in 2 weeks.";
+      break;
+    case "Ogden": 
+      msg = "Uh . . . where is that?";
+      break;
+    default:
+      msg = "Sorry! We do not deliver to your city yet.";
+  }
+}
+document.write(msg);
