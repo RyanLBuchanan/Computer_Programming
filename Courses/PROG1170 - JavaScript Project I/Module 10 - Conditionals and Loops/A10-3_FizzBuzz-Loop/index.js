306896046f7fb12bd590f01f419e576fa1a1aa9f
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-01-19 15:08:11 -0700

    feat: Completed PROG1170 Module 10 A10-3 FizzBuzz revision

--- Courses/PROG1170 - JavaScript Project I/Module 10 - Conditionals and Loops/A10-3_FizzBuzz-Loop/index.js
+++ Courses/PROG1170 - JavaScript Project I/Module 10 - Conditionals and Loops/A10-3_FizzBuzz-Loop/index.js
@@ -1,5 +1,6 @@
+
 function fizzBuzz(beginningNumber, endingNumber) {
-  for (var i = 1; i < endingNumber; i++) {
+  for (let i = 0 + beginningNumber; i <= endingNumber; i++) {
     if (i % 20 == 0) {
       document.write("FizzBuzz" + "<br>");
     }
@@ -18,3 +19,25 @@ function fizzBuzz(beginningNumber, endingNumber) {
 let beginningNumber = window.prompt("Please enter a beginning number: ", "");
 let endingNumber = window.prompt("Please enter a ending number: ", "");
 fizzBuzz(beginningNumber, endingNumber);
+
+
+// function fizzBuzz(beginningNumber, endingNumber) {
+//   for (var i = 1; i < endingNumber; i++) {
+//     if (i % 20 == 0) {
+//       document.write("FizzBuzz" + "<br>");
+//     }
+//     else if (i % 4 == 0) {
+//       document.write("Fizz" + "<br>");
+//     }
+//     else if (i % 5 == 0) {
+//       document.write("Buzz" + "<br>");
+//     }
+//     else {
+//       document.write(i + "<br>");
+//     }
+//   }
+// }
+
+// let beginningNumber = window.prompt("Please enter a beginning number: ", "");
+// let endingNumber = window.prompt("Please enter a ending number: ", "");
+// fizzBuzz(beginningNumber, endingNumber);
