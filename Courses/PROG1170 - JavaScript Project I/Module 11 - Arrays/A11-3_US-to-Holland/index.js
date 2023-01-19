a75fa3810a324d2a10ab8784d9f89336f22e8d87
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-01-19 10:45:14 -0700

    feat: Added PROG1170 Module 11 A11-1 Book Exercises

---
+++ Courses/PROG1170 - JavaScript Project I/Module 11 - Arrays/A11-3_US-to-Holland/index.js
@@ -0,0 +1,20 @@
+function fizzBuzz(beginningNumber, endingNumber) {
+  for (var i = 1; i < endingNumber; i++) {
+    if (i % 20 == 0) {
+      document.write("FizzBuzz" + "<br>");
+    }
+    else if (i % 4 == 0) {
+      document.write("Fizz" + "<br>");
+    }
+    else if (i % 5 == 0) {
+      document.write("Buzz" + "<br>");
+    }
+    else {
+      document.write(i + "<br>");
+    }
+  }
+}
+
+let beginningNumber = window.prompt("Please enter a beginning number: ", "");
+let endingNumber = window.prompt("Please enter a ending number: ", "");
+fizzBuzz(beginningNumber, endingNumber);
