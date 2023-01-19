d2a5e43b9be7ba949af9c7a235bd2e8f41142a14
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-01-18 20:20:32 -0700

    feat: Completed PROG1170 Module 11 Quiz

---
+++ Courses/PROG1170 - JavaScript Project I/Module 11 - Arrays/Exam/index.js
@@ -0,0 +1,32 @@
+// QUESTION 1
+// var thisArray = [ "apples", "peaches", "pumpkin", "pie" ];
+
+// document.write(thisArray.sort());
+
+// document.write(thisArray.join(","));
+
+// document.write(thisArray.toString());
+
+// document.write(thisArray.valueOf());
+
+// QUESTION 2
+// var dessert = [ "ice cream", "cake", "cookies", "pie" ];
+// var result = dessert.shift();
+// document.write(result);
+// document.write(dessert.toString());
+
+// QUESTION 3
+// var veggies = ["corn", "potato", "tomato", "broccoli"];
+
+// document.write(veggies);
+
+// QUESTION 4
+// var bugs = new Array[ "grasshopper", "cricket", "spider", "fly" ];
+
+var bugs = [ "grasshopper", "cricket", "spider", "fly" ];
+
+// var bugs = new Array(4);
+
+// var bugs = new Array( "grasshopper", "cricket", "spider", "fly" );
+
+document.write(bugs);
